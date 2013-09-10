4744a8cdf2789fab71e0b3e17781b3f1fbc2a667
Tobias Kuhn <kuhntobias@gmail.com>
2013-09-10 12:48:01 +0200

    List of apps

--- src/ch/ethz/iamscience/MainActivity.java
+++ src/ch/ethz/iamscience/MainActivity.java
@@ -1,15 +1,36 @@
 package ch.ethz.iamscience;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.AdapterView.OnItemClickListener;
 
 public class MainActivity extends Activity {
+
+    private ArrayAdapter<String> apps;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		apps = new ArrayAdapter<String>(this, R.layout.app_element);
+        ListView appListView = (ListView) findViewById(R.id.apps);
+        appListView.setAdapter(apps);
+        apps.add("nervous");
+        appListView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+				Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("ch.ethz.nervous");
+                startActivity(LaunchIntent);
+			}
+        	
+        });
 	}
 
 	@Override
